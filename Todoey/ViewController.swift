11052d56116fedae3f8f6dfffbd2096547b8f3d2
Sam Matinyan <sam.matinyan99@gmail.com>
2020-03-14 12:56:26 +0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Apple on 3/14/20.
+//  Copyright © 2020 ForZa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
